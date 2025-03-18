8dc454e8228df03acffc215d113b19097efb661b
JoaoAAnjos <joaoaganjos@gmail.com>
2025-03-18 13:48:54 +0100

    Align VSS and RTA algorithms so now they are both run without errors. Changed implementation of VSS so random computation time is calculated for each job, instead of each task

--- exercise.py
+++ exercise.py
@@ -2,9 +2,6 @@
 #       1. VSS Simulator
 #       2. RTA Analysis
 #       3. (previous to both) Response-time generator for tasks
-
-
-import numpy as np
 import pandas as pd
 import random
 import math
@@ -21,35 +18,14 @@ class Task:
         self.priority = priority
         #initialize it as -1 since this will be calculated by the simulator
         self.wcrt = -1
-        #initialize computation time with a random value between bcet and wcet using time_unit intervals
-        rd_values = [bcet + i * time_unit for i in range(int((wcet - bcet) // time_unit) + 1)]
-        self.comp_time = random.choice(rd_values)
 
 class Job:
-    def __init__(self, task_id: str, comp_time: int):
+    def __init__(self, task_id: str):
         self.task_id = task_id
-        self.exec_time = comp_time
         self.release_time = 0
         self.is_ready = True
-    
-#   Set of tasks: defined from 'data'
-class TaskSet:
-    global data, computation_times
-    
-    def __init__(self):
-        self.task_list = [Task() for _ in range(len(data))]
-        self.priority_order = []
+        self.exec_time = gen_random_comp_time(self)
 
-        temp_priority = data[:,4]
-
-        for i in range(len(data)):
-            self.task_list[i].comp_time = computation_times[i]
-            self.task_list[i].deadline = data[i,3]
-            self.task_list[i].id = f'T{i + 1}'
-
-        #   Get the priority order (RTA)
-        sorted_indices = sorted(range(len(temp_priority)), key=lambda i: temp_priority[i])
-        self.priority_order.extend(sorted_indices)
 
 #global variable so it can be accessed when creating the tasks
 time_unit = 0
@@ -60,31 +36,13 @@ tasks: Dict[str, Task] = {}
 #global variable for jobs
 jobs: List[Job] = []
 
-computation_times = []
-
 """
-This line will trigger an error with non-numeric values, so we cannot use this due to the first column of the csv (string).
-I had to comment the rest of the code that was not related to the simulator because I was getting a lot of errors related to this
-line when running the code, and I did not have the time to fix it (and all the other code is referencing it). 
-This line I know it will cause errors because of the column with strings, but the rest of the code is probably ok, I just
-commented it so I could test the simulator without these compilation errors.
-
-I don't think that the simulator is working 100% correctly at the moment, but I haven't been able to figure out whats wrong.
-However, I believe this implementation should be correct for the most part, and only needs small changes to work ok.
-
-UPDATE: uncommented this since I realized this will actually work, but I had to change data is loaded into here, or else it will have
-conflicts when launching vss_main, since it takes no sys args. This is just a temp solution, and the code itself is still broken by
-my changes, but I won't change anything else until further discussion
-"""
-data = None
-
-"""
-Responsible for handling the simulation is run using the information contained on the specified file
+Responsible for handling the VSS simulation is run using the information contained on the specified file
 """
 def run_vss(file_name: str, sim_time: int, time_unit: float):
     global current_time
 
-    print("Running simulation for " + file_name)
+    print("Running VSS simulation for " + file_name)
 
     #set the global variable for the time_unit
     globals()["time_unit"] = time_unit
@@ -127,16 +85,14 @@ def run_vss(file_name: str, sim_time: int, time_unit: float):
         
         current_time += time_unit
 
-    #output the results to the txt file
-    with open("results.txt", "w") as file:
-        file.write("Simulation results for application model in " + file_name + "\n")
+    #append the results to the txt file
+    with open("results-VSS.txt", "a") as file:
+        file.write("VSS Simulation results for application model in " + file_name + "\n")
 
         for key, value in tasks.items():
             file.write(key + ": " + str(value.wcrt) + "\n")
 
-        file.write("\n\n")
-
-    print("Simulation complete. Results have been outputed to results.txt")
+        file.write("\n\n")    
     
 
 """
@@ -169,11 +125,11 @@ def initialize_jobs():
 
     for task in tasks.values():
         job = Job(
-            task.id,
-            task.comp_time
+            task.id
         )
 
         jobs.append(job)
+
 
 """
 This function checks if a task needs to be activated. If a job has been executed inside its deadline, 
@@ -197,10 +153,11 @@ def activate_task_jobs():
                 if int(current_time)%task.period == 0:
                     #activate task job. Reset the values relevant for job execution
                     job.is_ready = True
-                    job.exec_time = task.comp_time
+                    job.exec_time = gen_random_comp_time(job)
                     job.release_time = 0
             else:
                 print("Corresponding Task for the job was not found. Something is wrong in the simulators execution.")
+
 
 """
 For the jobs that are ready, returns the one with the highest priority (i.e. whose corresponding task has the
@@ -221,43 +178,63 @@ def highest_priority_ready_job() -> Job:
 
     return result
 
-def gen_random_comp():
-    global data
-
-    for i in range(len(data)):
-        if int(data[i,1]) == int(data[i,0]):
-            computation_times.append(int(data[i,1]))
-        else:
-            #Switched the values here compared to main because you are starting at WCET and stoping at BECT which outputs an error
-            computation_times.append(random.randrange(int(data[i,0]),int(data[i,1]),1))
+"""
+Generates random computation time for a job
+"""
+def gen_random_comp_time(job : Job) -> float:
+    #get the corresponding task
+    task = tasks.get(job.task_id)
+    
+    return gen_random_comp_time_task(task)
     
 
-def RTA_analysis(set):
-    wcrt = []
+"""
+Generates random computation time for a task
+"""
+def gen_random_comp_time_task(task: Task) -> float:
+    #calculate computation time with a random value between bcet and wcet using time_unit intervals
+    rd_values = [task.bcet + i * time_unit for i in range(int((task.wcet - task.bcet) // time_unit) + 1)]
+    return random.choice(rd_values)
+
+"""
+Responsible for handling the RTA simulation is run using the information contained on the specified file
+"""
+def run_rta(file_name: str):
+    #set time_unit to 1 to ensure generation of comp time works without errors
+    global time_unit
+    time_unit = 1
+
     interference = 0
+    task_comp_times = {}
 
-    #   RTA algorithm
-    for i in range(len(set.priority_order)):
-        task = set.priority_order[i]
-        ri = set.task_list[task].comp_time / (1 - interference)
+    print("Running RTA simulation for " + file_name)
 
-        if ri > set.task_list[task].deadline:
-            wcrt.append(math.ceil(-1.0))
-            return wcrt
+    #create tasks from csv
+    initialize_tasks(pd.read_csv(file_name))
+
+    #generate random comp times for each task
+    for task in tasks.values():
+        task_comp_times[task.id] = gen_random_comp_time_task(task)
+
+    #sort tasks by priority
+    sorted_tasks = dict(sorted(tasks.items(), key=lambda item: item[1].priority))
+
+     #   RTA algorithm
+    for task in sorted_tasks.values():
+        ri = task_comp_times.get(task.id) / (1 - interference)
+
+        if ri > task.deadline:
+            task.wcrt =math.ceil(-1.0)
+            pass
         
-        wcrt.append(math.ceil(ri))
-        interference += (set.task_list[task].comp_time/set.task_list[task].deadline)
-    return wcrt
+        task.wcrt = math.ceil(ri)
+        interference += (task_comp_times.get(task.id)/task.deadline)
 
-def run_RTA(input):
-    global data
-    data = input
-    random.seed()
-    gen_random_comp()
+    #append the results to the txt file
+    with open("results-RTA.txt", "a") as file:
+        file.write("RTA Simulation results for application model in " + file_name + "\n")
 
-    #   Set creation
-    set1 = TaskSet()
+        for key, value in tasks.items():
+            file.write(key + ": " + str(value.wcrt) + "\n")
 
-    #   RTA call
-    wcrt_rta = RTA_analysis(set1)
-    print(wcrt_rta)
+        file.write("\n\n")    
--- vss_main.py
+++ vss_main.py
@@ -1,10 +1,10 @@
 import exercise as ex
 
-#Simulator start
+#VSS start
 print("""
-=========================================
-      Very Simple Simulator (VSS)
-=========================================
+===========================================
+      Very Simple Simulator (VSS) 
+===========================================
       
 The simulator takes as input the application model consisting of a set of hypothetical tasks stored 
 in a csv file and the simulation time. The output is a list of worst-case response times (WCRT) 
@@ -12,8 +12,8 @@ observed during the simulation for each task.
 
 This simulator expects the csv file to contain 6 columns, in the following order:
 -Task identifier
--Worst case execution time (WCET)
 -Best case execution time (BCET)
+-Worst case execution time (WCET)
 -Period
 -Deadline
 -Priority
@@ -27,14 +27,20 @@ and the file termination should be specified in the name (.csv).
 After defining the csv file(s) that will feed the simulation, you will be asked to input the simulation time.
 Keep in mind this is not a real time measure, but a theoretical time amount, used to define the number of cycles
 the simulator's logic will go through. Afterwards, you will be asked to input the desired time unit. This should
-be a numeric value, and not a standard time unit like seconds or miliseconds.
+be a numeric value, and not a standard time unit like seconds or miliseconds. If a float number is inputed, the
+separator should be (.)
+      
 The simulator will apply its logic for (simulation time / time unit) amount of times.
 
 If multiple csv files were passed, the same simulation time and time unit will be used for each simulation.
       
-The simulator will print the results to a txt file called 'results' 
+The simulator will print the results to a txt file called 'results-VSS' 
 (this can be improved, for example, by adding a column to the CSV)
 """)
+
+#clean results.txt file
+with open("results-VSS.txt", "w") as file:
+    pass
 
 #ask for csv file(s) containing the model and store the data
 csv_input = input("Specify the csv file(s) that contain the application model:")
@@ -46,6 +52,9 @@ sim_time = int(input("Input the desired simulation time:"))
 #ask for time unit value and store it
 time_unit = float(input("Input the desired time unit:"))
 
-#run the simulations
+#run the simulation(s)
 for file_name in csv_files:
     ex.run_vss(file_name, sim_time, time_unit)
+
+#simulation complete
+print("Simulation(s) complete. Results have been outputed to results-VSS.txt")
