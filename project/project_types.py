1656aaa2190a23108d627a057696abc0cd0d8338
caio_iriarte <caio.vaz137@gmail.com>
2025-04-11 15:06:43 +0200

    RT/Update class types

--- project/project_types.py
+++ project/project_types.py
@@ -1,26 +1,25 @@
 import random
-from typing import Dict,List
+from typing import Dict
 
 TIME_UNIT = 1
 
 
 #   ------------------------------------------------------------------------------------
-#   Task definition employed in HSS
+#   Task definitions employed in HSS
 class Task:
-    def __init__(self, id, wcet, bcet, period, deadline, priority):
+    def __init__(self, id, wcet, period, component_id, priority=-1):
         try:
             assert  type(id) == str and \
-                    type(wcet) == int and \
-                    type(bcet) == int and \
-                    type(period) == int and \
-                    type(deadline) == int and \
+                    type(wcet) == float and \
+                    type(period) == float and \
+                    type(component_id) == str and \
                     type(priority) == int
 
             self._id = id
             self._wcet = wcet
-            self._bcet = bcet
+            self._bcet = 0.0
             self._period = period
-            self._deadline = deadline
+            self._component_id = component_id
             self._priority = priority
 
             #   Initially define task as schedulable
@@ -41,6 +40,30 @@ def gen_random_comp_time_task(task: Task) -> float:
     rd_values = [task._bcet + i * TIME_UNIT for i in \
                  range(int((task._wcet - task._bcet) // TIME_UNIT) + 1)]
     return random.choice(rd_values)
+
+
+class Sporadic_task(Task):
+    def __init__(self,task,mit_time):
+        try:
+            assert isinstance(task,Task) and isinstance(mit_time,float)
+            self._task = task
+            self._mit_time = mit_time
+            pass
+        except AssertionError:
+            print("Error: Given parameters (Sporadic_task) \
+                  didn't meet the requirements for instance.")
+            
+
+class Periodic_task(Task):
+    def __init__(self,task,period):
+        try:
+            assert isinstance(task,Task) and isinstance(period,float)
+            self._task = task
+            self._period = period
+            pass
+        except AssertionError:
+            print("Error: Given parameters (Periodic_task) \
+                  didn't meet the requirements for instance.")
 
 
 #   ------------------------------------------------------------------------------------
@@ -76,17 +99,62 @@ def gen_random_comp_time(job : Job) -> float:
 
 
 #   ------------------------------------------------------------------------------------
+#   Resource Paradigm employed in HSS (only BDR model is valid)
+class Resource_paradigm:
+    def __init__(self,parameters,model="BDR"):
+        try:
+            #   Model definition:
+            #   * Bounded-Delay Resource (BDR)
+            #   * Periodic Resource Model (PRM)
+            #   * Explicit Deadline Periodic (EDP)
+            assert isinstance(model,str)
+            self._model = model
+
+            inst_valid = True
+
+            if model == "BDR" and len(parameters) == 2:
+                self._av_factor = parameters[0]
+                self._part_delay = parameters[1]
+            else:
+                inst_valid = False
+
+            #   In case of feasibility for PRM and EDP models, add:
+            """
+            elif model == "PRM" and len(parameters) == 2:
+                self._wcet = parameters[0]
+                self._period = parameters[1]
+            elif model == "EDP" and len(parameters) == 3:
+                self._wcet = parameters[0]
+                self._period = parameters[1]
+                self._deadline = parameters[2]
+            """
+
+            assert all(isinstance(inst,float) for inst in parameters) and inst_valid
+                
+        except AssertionError:
+            print("Error: Given parameters (Resource_paradigm) \
+                  didn't meet the requirements for instance.")
+        pass
+
+
+#   ------------------------------------------------------------------------------------
 #   Component employed in HSS 
 class Component:
-    def __init__(self,scheduler,terminal,interface,children=None):
+    def __init__(self,component_id,scheduler,budget,period,core_id,terminal,children=None):
         try:
-            #   Scheduler definition (string)
+            #   Component ID specification
+            assert type(component_id) == str
+            self._component_id = component_id
+
+            #   Core ID specification
+            assert type(core_id) == str
+            self._core_id = core_id
+            
+            #   Scheduler definition (string), can be:
+            #   * EDF (Earliest Deadline First)
+            #   * RM (Rate-Monotonic)
             assert type(scheduler) == str
             self._scheduler = scheduler
-
-            #   Interface definition (integer)
-            assert type(interface) == int
-            self._interface = interface
 
             #   Boolean variable which indicates the component is terminal
             #   (considered then a set of tasks) or not
@@ -103,9 +171,15 @@ class Component:
                 assert all(isinstance(inst, Component) for inst in children)
             self._sub_components = children
 
+            #   Interface definition (Half-half algorithm)
+            self._interface = None
+
+            if period > 0.0:    #   Security check for period
+                parameters = [float(budget/period),float(2*(period - budget))]
+                self._interface = Resource_paradigm(parameters)
+
             #   Check if component is schedulable
-            self._schedulable = None
-            self.is_schedulable()
+            self._schedulable = self.is_schedulable()
 
         except AssertionError:
             print("Error: Given parameters (Component) \
@@ -126,72 +200,34 @@ class Component:
     #   Compute if component is schedulable. Assume initially schedulable
     #   for empty tasks
     def is_schedulable(self):
-        if all(inst._schedulable for inst in self._sub_components):
-            self._schedulable = True
-        else:
-            self._schedulable = False
+        schedulable = False
 
-        return self._schedulable
+        if self._is_terminal:               #   Terminal component (set of tasks)
+            sumatory = 0.0
+            for task in self._sub_components:
+                #   Initially calculate by task._wcet instead of WCRT
+                sumatory += task._wcet/task._period
 
-
-#   ------------------------------------------------------------------------------------
-#   Resource Paradigm employed in HSS
-class Resource_paradigm:
-    def __init__(self,model,parameters):
-        try:
-            #   Model definition:
-            #   * Bounded-Delay Resource (BDR)
-            #   * Periodic Resource Model (PRM)
-            #   * Explicit Deadline Periodic (EDP)
-            assert isinstance(model,str)
-            self._model = model
-
-            inst_valid = True
-
-            if model == "BDR" and len(parameters) == 2:
-                self._av_factor = parameters[0]
-                self._part_delay = parameters[1]
-            elif model == "PRM" and len(parameters) == 2:
-                self._wcet = parameters[0]
-                self._period = parameters[1]
-            elif model == "EDP" and len(parameters) == 3:
-                self._wcet = parameters[0]
-                self._period = parameters[1]
-                self._deadline = parameters[2]
-            else:
-                inst_valid = False
-
-            assert all(isinstance(inst,float) for inst in parameters) and inst_valid
-                
-        except AssertionError:
-            print("Error: Given parameters (Resource_paradigm) \
-                  didn't meet the requirements for instance.")
-        pass
-
-
-#   ------------------------------------------------------------------------------------
-#   Scheduling unit employed in HSS
-class Scheduling_unit:
-    def __init__(self,root_comp,res_paradigm):
-        try:
-            #   Define root component (and sequence of components/tasks)
-            assert isinstance(root_comp,Component)
-            self._root_comp = root_comp
-
-            #   Define resource supply:
-            #   * Bounded-Delay Resource (BDR)
-            #   * Periodic Resource Model (PRM)
-            #   * Explicit Deadline Periodic (EDP)
-            assert isinstance(res_paradigm,Resource_paradigm)
-            self._res_paradigm = res_paradigm
+            if self._scheduler == "RM":     #   Rate-Monotonic scheduling
+                n = len(self._sub_components)
+                schedulable = sumatory <= n*(2**(1/n) - 1)
+            else:                           #   Earliest Deadline First scheduling
+                schedulable = sumatory <= 1.0
             
-        except AssertionError:
-            print("Error: Given parameters (Scheduling_unit) \
-                  didn't meet the requirements for instance.")
-        pass
+        return schedulable
 
-    
-    #   Check if unit is schedulable
-    def is_schedulable(self):
-        return self._root_comp.is_schedulable()
-    
+
+#   ------------------------------------------------------------------------------------
+#   Core class employed in HSS
+class Core():
+    def __init__(self,identifier,children):
+        try:
+            assert isinstance(identifier,str)
+            self._core_id = identifier
+
+            #   Create root component (0 interface)
+            self._root_comp = Component("Component_rt","EDF",0,0,self._core_id,False,children)
+
+        except AssertionError:
+            print("Error: Given parameters (Core) \
+                  didn't meet the requirements for instance.")
